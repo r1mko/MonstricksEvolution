3c96247a55fade21b523f681fd0c461cae91d71a
r1mko <r1mko@yahoo.com>
2026-04-27 00:59:42 +0400

    цены

--- Assets/Scripts/StoreManager.cs
+++ Assets/Scripts/StoreManager.cs
@@ -9,37 +9,7 @@ public class StoreManager : MonoBehaviour
     public class StoreItemData
     {
         public Image iconImage;
-        public TMP_Text nameText;
-        public TMP_Text descriptionText;
-        public Button buyButton;
-        public long unlockCost;
-
-        private string originalName;
-        private string originalDescription;
-
-        public void SaveOriginalData()
-        {
-            if (nameText != null) originalName = nameText.text;
-            if (descriptionText != null) originalDescription = descriptionText.text;
-
-            HideContent();
-        }
-
-        public void HideContent()
-        {
-            if (iconImage != null) iconImage.enabled = false;
-            if (nameText != null) nameText.text = "???";
-            if (descriptionText != null) descriptionText.text = "???";
-            if (buyButton != null) buyButton.interactable = false;
-        }
-
-        public void ShowContent()
-        {
-            if (iconImage != null) iconImage.enabled = true;
-            if (nameText != null && !string.IsNullOrEmpty(originalName)) nameText.text = originalName;
-            if (descriptionText != null && !string.IsNullOrEmpty(originalDescription)) descriptionText.text = originalDescription;
-            if (buyButton != null) buyButton.interactable = true;
-        }
+        public long priceButton;
     }
 
     [SerializeField] private List<StoreItemData> storeItems = new List<StoreItemData>();
@@ -64,50 +34,7 @@ public class StoreManager : MonoBehaviour
                 item.iconImage = iconTransform.GetComponent<Image>();
             }
 
-            Transform nameTransform = obj.transform.Find("NameText");
-            if (nameTransform != null)
-            {
-                item.nameText = nameTransform.GetComponent<TMP_Text>();
-            }
-
-            Transform descTransform = obj.transform.Find("DescriptionText");
-            if (descTransform != null)
-            {
-                item.descriptionText = descTransform.GetComponent<TMP_Text>();
-            }
-
-            Transform buttonTransform = obj.transform.Find("BuyButton");
-            if (buttonTransform != null)
-            {
-                item.buyButton = buttonTransform.GetComponent<Button>();
-            }
-
             storeItems.Add(item);
-        }
-
-        SaveAllOriginalData();
-    }
-
-    private void SaveAllOriginalData()
-    {
-        for (int i = 0; i < storeItems.Count; i++)
-        {
-            storeItems[i].SaveOriginalData();
-        }
-    }
-
-    public void UpdateItemVisuals()
-    {
-        for (int i = 0; i < storeItems.Count; i++)
-        {
-            if (playerMoney >= storeItems[i].unlockCost)
-            {
-                storeItems[i].ShowContent();
-            }
-            else
-            {
-                storeItems[i].HideContent();
-            }
         }
     }
 }
